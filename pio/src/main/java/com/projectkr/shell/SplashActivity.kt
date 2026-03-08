226243a219e09dc8377c02f8dea925005ecf8fdf
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-03-08 16:17:05 +0800

    migrate to compose

--- pio/src/main/java/com/projectkr/shell/SplashActivity.kt
+++ pio/src/main/java/com/projectkr/shell/SplashActivity.kt
@@ -151,7 +151,7 @@ class SplashActivity : ComponentActivity() {
 
         //  得到当前界面的装饰视图
         if (Build.VERSION.SDK_INT >= 21) {
-            val decorView = getWindow().getDecorView();
+            val decorView = window.getDecorView();
             //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
             val option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             decorView.setSystemUiVisibility(option);
